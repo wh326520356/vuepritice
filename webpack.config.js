5f50fc73b594ceaab0403818446ee9a96f5bc059
wh326520356 <326520356@qq.com>
2019-05-14 21:31:48 +0800

    webpack 练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-plugin');
 const webpack = require('webpack');
-const config = {
-    entry: './src/index.js',
+
+module.exports = {
+    entry:{
+        index: path.resolve(__dirname,'./src/index.js'),
+        vendors: ['vue','vue-router']
+    },
     output: {
-      filename: 'bundlue.js',
-      path: __dirname+'/dist'
+        path: path.resolve(__dirname,"./dist/js"),
+        filename: "[name].[hash].js",
+        chunkFilename: "[id].[chunkhash].js"
+    },
+    resolve: {
+        extensions: ['','.js','.vue'],
+        alias: {
+        }
     },
     module: {
-        rules: [
+        loaders: [
             {
-                test: /\.txt$/,
-                loader: 'raw-loader'
-            },
-            {
-                test:/\.less$/,
-                loader: "style-loader!css-loader!less-loader"
+                test: /\.vue$/,
+                loader: 'vue-loader'
             }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template:'./src/index.html'})
-    ]
+    }
 }
-module.exports = config;
