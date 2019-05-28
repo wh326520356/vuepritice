434d72c0f84d991fd51cabc22d1dc85322d5c599
wh326520356 <326520356@qq.com>
2019-05-28 22:37:16 +0800

    添加radio组件

---
+++ src/common/wh-radio.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wh-radio">
+        <label><input v-model="radioValue" :name="name" type="radio"/><span></span><slot></slot></label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Radio",
+        props: {
+            name: {
+                type: String,
+                default: ``
+            }
+        },
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,19 @@
     <div class="home">
         <span>123</span>
         <Button :buttonClass="homeButton">点击</Button>
+        <Radio name="num">1</Radio>
+        <Radio name="num">2</Radio>
     </div>
 </template>
 
 <script>
+    import Radio from "../common/wh-radio.vue"
     import Button from "../common/wh-button.vue"
     export default {
         name: "Home",
         components: {
-            Button
+            Button,
+            Radio
         },
         data() {
             return {
